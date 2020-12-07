ae31224b752ee0befdce5d17c10a1cfec41887a8
Kuba Klimas <kuba.klimas1@gmail.com>
2020-12-07 17:15:47 +0100

    Sending message, repairs voice recognition

--- speech_rec.py
+++ speech_rec.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import reply_voice as reply
 import time
 
-
+'''
 while True:   
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -15,14 +15,17 @@ while True:
             time.sleep(5)
         except:
             reply.speak("I do not understand")
-
-
 '''
-
-Długi czas oczekiwania na jego reply
-możliwe powody:
-    - r.recognize dlugo sie laduje 
-    - funkcja speak zapisuje do pliku .mp3 a nastepnie go usuwa, może nie 
-    nadążać za nowymi funkcjami 
-
-'''
+def rec_voice():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio,language="en-EN")
+            print("You said : {}".format(text))
+            return text
+            
+        except:
+            print('I did not recognize')
+            #reply.speak("I do not understand")
